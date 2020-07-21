fa8da5446dfb8ac7f16d906db900661ba5c65e1e
Cluas <Cluas@live.cn>
2020-07-21 16:37:48 +0800

    fix: order slice for correct test

--- 0040/code_test.go
+++ 0040/code_test.go
@@ -11,13 +11,13 @@ func TestCombinationSum2(t *testing.T) {
 		target       int
 		combinations [][]int
 	}{
-		{[]int{10, 1, 2, 7, 6, 1, 5}, 8, [][]int{{1, 2, 5}, {1, 7}, {1, 1, 6}, {2, 6}}},
+		{[]int{10, 1, 2, 7, 6, 1, 5}, 8, [][]int{{1, 1, 6}, {1, 2, 5}, {1, 7}, {2, 6}}},
 		{[]int{2, 5, 2, 1, 2}, 5, [][]int{{1, 2, 2}, {5}}},
 	}
 
 	for _, tt := range tests {
 		combinations := combinationSum2(tt.candidates, tt.target)
-		if reflect.DeepEqual(combinations, tt.combinations) == false {
+		if !reflect.DeepEqual(combinations, tt.combinations) {
 			t.Errorf("combinationSum2(%v, %v) return %v, want %v", tt.candidates, tt.target, combinations, tt.combinations)
 		}
 	}
